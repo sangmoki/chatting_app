d6817667a14f87f9fd131b1faef48473f35325f7
이상목 <yoochang4176@naver.com>
2024-06-18 11:50:49 +0900

    Feat: "Groupy 라이브러리 지원 중단으로 인한 Adapter 생성"

---
+++ app/src/main/java/com/sangmoki/chatting_app/RvAdapter.kt
@@ -0,0 +1,33 @@
+package com.sangmoki.chatting_app
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RvAdapter(val items : MutableList<String>) : RecyclerView.Adapter<RvAdapter.ViewHolder>() {
+
+    // 각 RecyclerView의 아이템 뿌려주기
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_item_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    // ViewHolder의 데이터 바인딩 처리
+    override fun onBindViewHolder(holder: RvAdapter.ViewHolder, position: Int) {
+        holder.bindItems(items[position])
+    }
+
+    // 전체 RecyclerView 개수
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    // ViewHolder 생성
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(item : String) {
+
+        }
+    }
+}
